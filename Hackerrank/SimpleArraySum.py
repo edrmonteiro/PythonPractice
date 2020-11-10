9178f5c2b33403732cb34d934616f74e9d59e742
Eduardo Monteiro <eduardormonteiro@gmail.com>
2020-11-10 11:36:28 -0300

    add Hackerrank SimpleArraySum

---
+++ Hackerrank/SimpleArraySum.py
@@ -0,0 +1,29 @@
+"""
+coding: utf-8
+Created on 09/11/2020
+
+@author: github.com/edrmonteiro
+
+From: Hackrank challenges
+Language: Python
+
+"""
+
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    return sum(ar)
+
+ar = [5, 6, 4, 3]
+
+stop = True
