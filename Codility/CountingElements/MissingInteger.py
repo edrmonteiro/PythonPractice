608b5a60063d9fcdeaad3e9cbde43e2dc5bc97f9
eduardo <eduardormonteiro@gmail.com>
2021-01-28 14:54:03 -0300

    Codility - CountingElements - MissingInteger

---
+++ Codility/CountingElements/MissingInteger.py
@@ -0,0 +1,22 @@
+"""
+This is a demo task.
+Write a function:
+def solution(A)
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+Given A = [1, 2, 3], the function should return 4.
+Given A = [−1, −3], the function should return 1.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+"""
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    completeSet = set(range(1, len(A)+2))
+    return min(completeSet.difference(set(A)))
+
+
