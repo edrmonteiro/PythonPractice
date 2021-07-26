f6b7d62198083acd1e26bcab7068f301e4dbee34
eduardormonteiro@gmail.com <eduardormonteiro@gmail.com>
2021-07-26 14:13:37 -0300

    scrapy models and pipelines

---
+++ scrapy/4_models_pipelines/quotes_spider/quotes_spider/spiders/quotes.py
@@ -0,0 +1,22 @@
+from scrapy import Spider
+from scrapy.loader import ItemLoader
+
+from quotes_spider.items import QuotesSpiderItem
+
+class QuotesSpider(Spider):
+    name = 'quotes'
+    allowed_domains = ['http://quotes.toscrape.com/']
+    start_urls = (
+        'http://quotes.toscrape.com/',
+    )
+
+    def parse(self, response):
+        l = ItemLoader(item=QuotesSpiderItem(), response=response)
+        h1_tag = response.xpath('//h1/a/text()').extract()[0]
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+
+        l.add_value('h1_tag', h1_tag)
+        l.add_value('tags', tags)
+
+        #yield {'H1 Tag': h1_tag, 'Tags':tags}
+        return l.load_item()
